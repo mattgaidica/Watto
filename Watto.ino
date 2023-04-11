6b4f19749ee35aa1102c8823edba5e466a03086e
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-04-11 14:54:56 -0400

    removed notion of buffer, just streams now

--- Watto.ino
+++ Watto.ino
@@ -5,20 +5,24 @@
 #include <ArduinoBLE.h>
 #include <INA226.h>
 #include <Wire.h>
+#include <math.h>
 
-const size_t MAX_BUFFER_SIZE = 200; // 200 * 5ms = 1s
+// set these up to minimize optimize delay for conversion
+const int USE_AVG = 1;
+const int USE_SVCT = 4;
+const int averageTable[8] = { 1, 4, 16, 64, 128, 256, 512, 1024 };                                      // samples
+const int conversionTable[8] = { 140, 204, 332, 588, 1.1 * 1000, 2.1 * 1000, 4.2 * 1000, 8.3 * 1000 };  // us
+float intermediateValue = (averageTable[USE_AVG] * conversionTable[USE_SVCT]) / 1000.0;
+int loopDelayMs = ceil(intermediateValue);
 
 INA226 INA(0x40);
 
 BLEService customService("A000");
-BLECharacteristic voltageCharacteristic("A001", BLERead | BLENotify, MAX_BUFFER_SIZE);
-BLECharacteristic currentCharacteristic("A002", BLERead | BLENotify, MAX_BUFFER_SIZE);
-BLECharacteristic powerCharacteristic("A003", BLERead | BLENotify, MAX_BUFFER_SIZE);
+BLECharacteristic voltageCharacteristic("A001", BLERead | BLENotify, sizeof(float));
+BLECharacteristic currentCharacteristic("A002", BLERead | BLENotify, sizeof(float));
+BLECharacteristic powerCharacteristic("A003", BLERead | BLENotify, sizeof(float));
 
-float uCurrent, uVoltage, uPower;
-uint8_t voltageData[MAX_BUFFER_SIZE];
-uint8_t currentData[MAX_BUFFER_SIZE];
-uint8_t powerData[MAX_BUFFER_SIZE];
+float uVoltage = 0, uCurrent = 0, uPower = 0;
 size_t dataSize = 0;
 unsigned long lastSentTime = 0;
 
@@ -27,28 +31,28 @@ void setup() {
   // while (!Serial);
 
   Wire.begin();
-  if (!INA.begin() )
-  {
+  if (!INA.begin()) {
     Serial.println("could not connect. Fix and Reboot");
   }
-  // 20mA, 0.1ohm
-  INA.setMaxCurrentShunt(0.01, 0.1);
-  INA.setAverage(1);
-  INA.setShuntVoltageConversionTime(3);
+ 
+  INA.setMaxCurrentShunt(0.05, 0.115); // R=0.1ohm but empirically slightly different
+  INA.setAverage(USE_AVG);
+  INA.setShuntVoltageConversionTime(USE_SVCT);
 
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
-    while (1);
+    while (1)
+      ;
   }
 
-  BLE.setLocalName("WATTO");
+  BLE.setLocalName("Watto");
+  BLE.setDeviceName("Watto");
   BLE.setAdvertisedService(customService);
   customService.addCharacteristic(voltageCharacteristic);
   customService.addCharacteristic(currentCharacteristic);
   customService.addCharacteristic(powerCharacteristic);
   BLE.addService(customService);
-  uint8_t emptyData[MAX_BUFFER_SIZE] = {0};
-  currentCharacteristic.writeValue(emptyData, MAX_BUFFER_SIZE);
+  currentCharacteristic.writeValue(&uVoltage, sizeof(uVoltage));
   BLE.advertise();
 
   Serial.println("BLE Peripheral - Current Sensor");
@@ -64,50 +68,18 @@ void loop() {
     dataSize = 0;
 
     while (central.connected()) {
-      // if (dataSize == MAX_BUFFER_SIZE) {
-      //   sendData();
-      //   dataSize = 0;
-      // }
+      uVoltage = INA.getShuntVoltage_uV();
+      uCurrent = INA.getCurrent_uA();
+      // uPower = INA.getPower_uW(); // not working
+      uPower = uVoltage * uCurrent;
 
-      // if (dataSize < MAX_BUFFER_SIZE) {
-        uVoltage = INA.getShuntVoltage_uV();
-        uCurrent = INA.getCurrent_uA();
-        // uPower = INA.getPower_uW();
-        uPower = uVoltage * uCurrent;
-
-      voltageCharacteristic.writeValue(&uVoltage, sizeof(float));
+      // voltageCharacteristic.writeValue(&uVoltage, sizeof(float));
       currentCharacteristic.writeValue(&uCurrent, sizeof(float));
       powerCharacteristic.writeValue(&uPower, sizeof(float));
-
-        // memcpy(&voltageData[dataSize], &uVoltage, sizeof(float));
-        // memcpy(&currentData[dataSize], &uCurrent, sizeof(float));
-        // memcpy(&powerData[dataSize], &uPower, sizeof(float));
-        // dataSize += sizeof(float);
-        delay(3);
-      // }
+      delay(loopDelayMs);
     }
 
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
-  }
-}
-
-void sendData() {
-  if (dataSize == 0) return;
-
-  size_t remainingData = dataSize;
-  uint8_t* voltageDataPtr = voltageData;
-  uint8_t* currentDataPtr = currentData;
-  uint8_t* powerDataPtr = powerData;
-
-  while (remainingData > 0) {
-    size_t notifSize = min((size_t)20, remainingData); // Max 20 bytes per notification
-    voltageCharacteristic.writeValue(voltageDataPtr, notifSize);
-    currentCharacteristic.writeValue(currentDataPtr, notifSize);
-    powerCharacteristic.writeValue(powerDataPtr, notifSize);
-    voltageDataPtr += notifSize;
-    currentDataPtr += notifSize;
-    powerDataPtr += notifSize;
-    remainingData -= notifSize;
   }
 }
