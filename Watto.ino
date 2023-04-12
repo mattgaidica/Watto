62198c0ca35536e3327a4ffeb42e4603c64f592f
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-04-12 10:43:32 -0400

    Options to only broadcast required data

--- Watto.ino
+++ Watto.ino
@@ -1,21 +1,25 @@
 // by: Matt Gaidica
-
-#define VERSION 230410
+// ...many thanks Rob Tillaart for the INA226 library
 
 #include <ArduinoBLE.h>
 #include <INA226.h>
 #include <Wire.h>
 #include <math.h>
 
-// set these up to minimize optimize delay for conversion
+#define VERSION 230412 // yymmdd
+INA226 INA(0x40);
+
+// always send current, others are opt to make loop fast
+const bool SEND_VOLTAGE = false;
+const bool SEND_POWER = false;
+
+// set these up to minimize optimize delay for conversion, see INA226 data sheet
 const int USE_AVG = 1;
 const int USE_SVCT = 4;
 const int averageTable[8] = { 1, 4, 16, 64, 128, 256, 512, 1024 };                                      // samples
 const int conversionTable[8] = { 140, 204, 332, 588, 1.1 * 1000, 2.1 * 1000, 4.2 * 1000, 8.3 * 1000 };  // us
-float intermediateValue = (averageTable[USE_AVG] * conversionTable[USE_SVCT]) / 1000.0;
-int loopDelayMs = ceil(intermediateValue);
-
-INA226 INA(0x40);
+float floatDelay = (averageTable[USE_AVG] * conversionTable[USE_SVCT]) / 1000.0;
+int loopDelayMs = ceil(floatDelay);
 
 BLEService customService("A000");
 BLECharacteristic voltageCharacteristic("A001", BLERead | BLENotify, sizeof(float));
@@ -23,8 +27,6 @@ BLECharacteristic currentCharacteristic("A002", BLERead | BLENotify, sizeof(floa
 BLECharacteristic powerCharacteristic("A003", BLERead | BLENotify, sizeof(float));
 
 float uVoltage = 0, uCurrent = 0, uPower = 0;
-size_t dataSize = 0;
-unsigned long lastSentTime = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -34,8 +36,8 @@ void setup() {
   if (!INA.begin()) {
     Serial.println("could not connect. Fix and Reboot");
   }
- 
-  INA.setMaxCurrentShunt(0.05, 0.115); // R=0.1ohm but empirically slightly different
+
+  INA.setMaxCurrentShunt(0.05, 0.115);  // R=0.1ohm but empirically slightly different
   INA.setAverage(USE_AVG);
   INA.setShuntVoltageConversionTime(USE_SVCT);
 
@@ -55,7 +57,7 @@ void setup() {
   currentCharacteristic.writeValue(&uVoltage, sizeof(uVoltage));
   BLE.advertise();
 
-  Serial.println("BLE Peripheral - Current Sensor");
+  Serial.println("BLE Peripheral - Watto");
 }
 
 void loop() {
@@ -64,19 +66,23 @@ void loop() {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
 
-    lastSentTime = millis();
-    dataSize = 0;
-
     while (central.connected()) {
-      uVoltage = INA.getShuntVoltage_uV();
       uCurrent = INA.getCurrent_uA();
-      // uPower = INA.getPower_uW(); // not working
-      uPower = uVoltage * uCurrent;
-
-      // voltageCharacteristic.writeValue(&uVoltage, sizeof(float));
       currentCharacteristic.writeValue(&uCurrent, sizeof(float));
-      powerCharacteristic.writeValue(&uPower, sizeof(float));
-      delay(loopDelayMs);
+
+      if (SEND_VOLTAGE || SEND_POWER) {
+        uVoltage = INA.getShuntVoltage_uV();
+        uPower = uVoltage * uCurrent;
+        // uPower = INA.getPower_uW(); // not working
+        if (SEND_VOLTAGE) {
+          voltageCharacteristic.writeValue(&uVoltage, sizeof(float));
+        }
+        if (SEND_POWER) {
+          powerCharacteristic.writeValue(&uPower, sizeof(float));
+        }
+      }
+      // delays minimal amount based on averaging/conversion time, but may be yoked by BLE
+      delay(loopDelayMs); 
     }
 
     Serial.print("Disconnected from central: ");
